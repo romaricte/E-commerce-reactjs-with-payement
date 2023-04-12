f3717df8f6e84a494e2e6249ff9ebef15b649929
Tene Romaric <tenrom142@gmail.com>
2023-04-12 13:01:45 +0100

    second commit

---
+++ src/assets/Index.js
@@ -0,0 +1,8 @@
+import cart from './cart.png'
+import githubLogo from './githubLogo.png'
+import googleLogo from './googleLogo.png'
+import logoDark from './logoDark.png'
+import logoLight from './logoLight.png'
+import paymentLogo from './paymentLogo.png'
+
+export{paymentLogo, logoLight, logoDark, googleLogo, githubLogo, cart}
